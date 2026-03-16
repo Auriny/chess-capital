aca02d8f0c1f19c8747cdbb9cd7d819c2fc9120b
S1lvestr <noban8520@gmail.com>
2026-03-17 05:56:17 +0600

    docs: Fix openapi "BAckend" to "Backend"

--- game-service/main.py
+++ game-service/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 from routers import api_router
 
-app = FastAPI(title="Chess Capital BAckend")
+app = FastAPI(title="Chess Capital Backend")
 
 app.include_router(api_router)
